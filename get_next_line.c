9df41a16e1b3de72a87670af5410557ce8cad58d
siil3omary <132150526+siil3omary@users.noreply.github.com>
2023-12-01 15:16:56 +0100

    done

--- get_next_line.c
+++ get_next_line.c
@@ -1,14 +1,98 @@
 #include "get_next_line.h"
+#define BUFFER_SIZE 5
+
+char *read_buffer(int fd ,char *buf)
+{
+int read_byte;
+char *readed_buf;
+
+
+readed_buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+if (!readed_buf)
+{
+	return (NULL);
+}
+
+while (1 != 2)
+{
+	read_byte = read(fd, readed_buf, BUFFER_SIZE);
+	if (read_byte == -1)
+		return (NULL);
+	read_buffer[read_byte] = '\0';
+	if (read_byte == 0)
+		break;
+	buf = ft_strjoin(buf , readed_buf);
+	if (isnewline(buf))
+		break;
+	
+}
+free(readed_buf);
+return (buf);
+
+
+
+}
+
+char *extract_line(char *buf)
+{
+	char *line;
+
+	int i;
+	i = 0;
+	if (!buf || ft_strlen(buf) == 0)
+	{
+		return (NULL);
+	}
+	while (buf[i] && buf[i] != '\n')
+	i++;
+
+	line = ft_substr(buf, 0, i + 1);
+	return (line);
+	
+}
+
+char *new_buf(char *buf)
+{
+	char *new;
+	int buf_lenght;
+	if (!buf)
+	{
+		return (NULL);
+	}
+	
+	buf_lenght = ft_strlen(buf);
+		while (buf[i] && buf[i] != '\n')
+		i++;
+		buf_lenght = i;
+		while (buf[buf_lenght])
+		{
+			buf_lenght++;
+		}
+		buf_lenght -= i;
+
+	new = ft_substr(buf, i + 1 , buf_lenght);
+	 free(buf);
+	 return (new);
+
+}
 
 char *get_next_line(int fd)
 {
-static char *line = NULL;
+ char *line = NULL;
 
+static char *buf = NULL;
 
 if(fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 	return (NULL);
 
 
 
+buf = read_buff(fd , buf);
+if(!buf)
+return (NULL);
+line = extract_line(buf);
+buf = new_buf(buf);
 
+
+return (line);
 }
---
+++ get_next_line_utils.c
@@ -0,0 +1,101 @@
+#include "get_next_line.h"
+
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	lenght;
+
+	lenght = 0;
+	while (s[lenght])
+		lenght++;
+	return (lenght);
+}
+
+
+char	*ft_strdup(const char *s)
+{
+	char	*ptr;
+
+	ptr = (char *)malloc(ft_strlen(s) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	ft_strlcpy(ptr, s, ft_strlen(s) + 1);
+	return (ptr);
+}
+
+
+
+
+static char	*constr(char const *s1, char const *s2)
+{
+	size_t	all_len;
+	char	*result;
+	size_t	i;
+	size_t	ri;
+
+	all_len = ft_strlen(s1) + ft_strlen(s2);
+	result = (char *)malloc((all_len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	ri = 0;
+	i = 0;
+	while (s1[i])
+		result[ri++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		result[ri++] = s2[i++];
+	result[ri] = '\0';
+	return (result);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	if (!s1 || !s2)
+	{
+		if (s1)
+			return (ft_strdup(s1));
+		else if (s2)
+			return (ft_strdup(s2));
+		else
+			return (NULL);
+	}
+	return (constr(s1, s2));
+}
+int isnewline(char *buf)
+{
+		int	i;
+
+	i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	str_len;
+	char	*ptr;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	str_len = ft_strlen(s);
+	if (start >= str_len || len == 0)
+		return (ft_strdup(""));
+	if (len > str_len - start)
+		len = str_len - start;
+	ptr = (char *)malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
