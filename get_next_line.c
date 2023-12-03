b2ce510f3c207ced8bf7afff2e7f11a0031e7fe6
Mona Lisa <l3omary@icloud.com>
2023-12-03 21:08:41 +0100

    done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aelomari <aelomari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:18:29 by aelomari          #+#    #+#             */
-/*   Updated: 2023/12/03 21:06:56 by aelomari         ###   ########.fr       */
+/*   Updated: 2023/12/03 21:08:25 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,7 @@ char	*read_buffer(int fd, char *buf)
 	{
 		read_byte = read(fd, readed_buf, BUFFER_SIZE);
 		if (read_byte == -1)
-		{
-
 			return (NULL);
-		}
 		readed_buf[read_byte] = '\0';
 		if (read_byte == 0)
 			break ;
