64813b59ebce990ba49107380970fd80992bc7dc
siil3omary <132150526+siil3omary@users.noreply.github.com>
2023-12-04 15:09:28 +0100

    hello

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelomari <aelomari@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:18:32 by aelomari          #+#    #+#             */
-/*   Updated: 2023/12/03 20:24:15 by aelomari         ###   ########.fr       */
+/*   Updated: 2023/12/04 14:59:55 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ char	*ft_strdup(const char *s)
 	size_t	i;
 
 	i = 0;
+
 	ptr = (char *)malloc(ft_strlen(s) + 1);
 	if (ptr == NULL)
 		return (NULL);
--- main.c
+++ main.c
@@ -1,26 +1,21 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	main(void)
+int main()
 {
 	int fd;
 	char *line;
+	int i;
 
+	i = 0;
 	fd = open("text.txt", O_RDONLY);
-	// Replace "test.txt" with the path to your test file
-	if (fd == -1)
-	{
-		printf("Failed to open the file.\n");
-		return (1);
-	}
+	line = get_next_line(fd);
+	
+	printf("%d\n", fd);
+	printf("%s", line);
 
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s\n", line);
-		free(line);
-	}
-
+	
+free(line);
 	close(fd);
-
 	return (0);
 }
